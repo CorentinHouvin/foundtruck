bc4ef00fc93237d11f69a54cf6141386d7ce024a
CorentinHouvin <houvin.corentin@gmail.com>
2020-04-17 15:38:57 +0200

    environment prod update

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:3000/api",
+  apiBaseUrl: "http://134.209.198.134:5566/api",
   mapbox: {
     accessToken:
       "pk.eyJ1IjoiY29yZW50aW5ob3V2aW4iLCJhIjoiY2s2czB1bm1jMGF1MTNkbXFiOXIwYXVldiJ9.jS1nAFv69CumsXQleroTnw",
